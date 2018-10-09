21bb3c42c7cc240e5da6c1d18719b84c0aa64588
Kavindu Chinthana <it14136252@my.sliit.lk>
2018-10-09 19:35:26 +0530

    Bootstrap added

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="./bootstrap/bootstrap.min.css">
+</head>
+
+<body>
+    <div class="btn btn-primary"> Start Uploading </div>
+    <script src="./bootstrap/bootstrap.min.js">
+    </script>
+</body>
+
+</html>
